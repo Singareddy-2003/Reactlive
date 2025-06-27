99f01412146cf3263e840cec8c4ed9fb11c1b055
Singareddy-2003 <mahanthsingareddy3@gmail.com>
2025-06-27 15:39:50 +0530

    completed deployament of react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+let api_key = import.meta.VITE_API_KEY
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Welcome to react </h1>
+    </div>
+  )
+}
+
+export default App
